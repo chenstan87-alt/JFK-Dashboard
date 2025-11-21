6421b72b388230efc67719b780db847c21345260
chenstan87-alt <chenstan87@gmail.com>
2025-11-20 21:27:45 -0500

    Add files via upload

--- JFK_dashboard.py
+++ JFK_dashboard.py
@@ -10,8 +10,13 @@ daily_summary_ata = get_daily_summary_ata()
 # 自动加载最新数据（已带缓存和自动刷新）
 overall_data, weekly_analysis, recent4, duration_analysis = get_all_data()
 
-daily_summary_ata_ = daily_summary_ata.sort_values(by='ata_date', ascending=False).head(10)
-daily_summary_ata_.rename(columns={'延误-严重': '延误-严重（超过24小时）'}, inplace=True)
+daily_summary_ata_=daily_summary_ata.sort_values(by='ata_date',ascending=False).head(10)
+daily_summary_ata_.rename(columns={'延误-严重':'延误-严重（超过24小时）'},inplace=True)
+
+overall_data_=overall_data.sort_values(by='操作日期',ascending=False)
+weekly_analysis_=weekly_analysis.sort_values(by='Monday',ascending=False)
+recent4_=recent4.sort_values(by='Monday',ascending=False)
+duration_analysis_=duration_analysis.sort_values(by='操作日期',ascending=False)
 
 st.expander("一、MIRA时效").dataframe(daily_summary_ata_)
 st.markdown("---")
